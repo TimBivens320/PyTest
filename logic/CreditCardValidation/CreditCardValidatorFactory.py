16b09773d42c0c88bd322f4b6000305cba0b1de4
bivenst <tbivens320@gmail.com>
2017-01-12 13:49:14 -0500

    adding CreditCardType

--- logic/CreditCardValidation/CreditCardValidatorFactory.py
+++ logic/CreditCardValidation/CreditCardValidatorFactory.py
@@ -5,7 +5,7 @@ from objectmodel.enums.CreditCardType import CreditCardType
 
 class CreditCardValidatorFactory(object):
     @staticmethod
-    def get(credit_card_type):
+    def get(credit_card):
         validation_services = {CreditCardType.VISA: VisaValidator,
                                CreditCardType.MASTERCARD: MasterCardValidator}
-        return validation_services[credit_card_type]
+        return validation_services[credit_card.credit_card_type]
---
+++ objectmodel/CreditCard.py
@@ -0,0 +1,3 @@
+class CreditCard:
+    def __init__(self, credit_card_type):
+        self.credit_card_type = credit_card_type
